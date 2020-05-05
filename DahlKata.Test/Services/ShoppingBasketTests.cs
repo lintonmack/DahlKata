4f200a675e29c866ef058362a01f54b038db7348
lintonmack <lintonmackereth@gmail.com>
2020-05-05 09:04:32 +0100

    Add first failing test for ShouldReturnNoDiscountForOneBook()

---
+++ DahlKata.Test/Services/ShoppingBasketTests.cs
@@ -0,0 +1,33 @@
+﻿using DahlKata.Core.Services;
+using DahlKata.Core.Models;
+using Xunit;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace DahlKata.Test.Services
+{
+    public class ShoppingBasketTests
+    {
+        [Fact]
+        public void ShouldReturnNoDiscountForOneBook()
+        {
+            // Arrange
+            var author = new Author("Roald Dahl");
+
+            var book = new List<Book>
+            {
+                new Book("Matilda", author, 8)
+            };
+
+            var subject = new ShoppingBasket(book);
+
+            // Act
+
+            var result = subject.CalculateShoppingBasketTotal();
+
+            // Assert
+            result.Should().Be(8);
+        }
+
+    }
+}
