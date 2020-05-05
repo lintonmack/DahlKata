2ba356ba7f7cd93fcad9f8b487add46520feb42c
lintonmack <lintonmackereth@gmail.com>
2020-05-05 11:11:30 +0100

    Added failing test for ShouldReturnTwentyFivePercentDiscountForFiveDifferentBooks()

--- DahlKata.Test/Services/ShoppingBasketTests.cs
+++ DahlKata.Test/Services/ShoppingBasketTests.cs
@@ -93,5 +93,29 @@ namespace DahlKata.Test.Services
             // Assert
             result.Should().Be(25.60);
         }
+
+        [Fact]
+        public void ShouldReturnTwentyFivePercentDiscountForFiveDifferentBooks()
+        {
+            // Arrange
+            var author = new Author("Roald Dahl");
+
+            var book = new List<Book>
+            {
+                new Book("Matilda", author, 8),
+                new Book("The BFG", author, 8),
+                new Book("The Witches", author, 8),
+                new Book("Boy", author, 8),
+                new Book("James & The Giant Peach", author, 8)
+            };
+
+            var subject = new ShoppingBasket(book);
+
+            // Act
+            var result = subject.CalculateShoppingBasketTotal();
+
+            // Assert
+            result.Should().Be(30.00);
+        }
     }
 }
